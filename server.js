a15f59699e4d99c020f28a2d92f2d1748c501d57
Brian <briand.nester@gmail.com>
2021-03-05 13:34:04 -0500

    Change Port code

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/hackeramp/index.html'));
 });
 
-const port = process.env.PORT || '3000';
-app.set('port', port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 const server = http.createServer(app);
 
