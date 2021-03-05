cf31695bd43ede6dcaa82755ab6790eade651eda
Brian <briand.nester@gmail.com>
2021-03-05 14:03:08 -0500

    push

--- server.js
+++ server.js
@@ -24,7 +24,5 @@ app.get('*', (req, res) => {
 
 // server.listen(port, () => console.log(`Running on localhost:${port}`));
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 3000);
 
