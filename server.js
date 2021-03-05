89297a3637f097079ae7c6f018f578820d095482
Brian <briand.nester@gmail.com>
2021-03-05 13:38:46 -0500

    Deploy error fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/hackeramp/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
