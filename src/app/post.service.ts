047d21c079cde75c41d6f94a5515267560a7786a
Brian <briand.nester@gmail.com>
2021-03-05 14:59:30 -0500

    push

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -13,12 +13,12 @@ export class PostService {
   constructor(private _http: HttpClient) { }
 
   getPosts() {
-    return this._http.get("api/posts")
+    return this._http.get("https://hackeramp.herokuapp.com/api/posts")
       .map(result => this.result = result);
   }
 
   getPost(id) {
-    return this._http.get("api/details/"+id)
+    return this._http.get("https://hackeramp.herokuapp.com/api/details/"+id)
       .map(result => this.result = result);
   }
 
@@ -26,7 +26,7 @@ export class PostService {
     let headers = new HttpHeaders({ 'Content-Type': 'application/json'});
     let options = { headers: headers };
 
-    return this._http.post("api/posts", JSON.stringify(post), options)
+    return this._http.post("https://hackeramp.herokuapp.com/api/posts", JSON.stringify(post), options)
     .map(result => this.result = result);
     
   }
