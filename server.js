0e28d35882efa3f1ab88815a71483e3faa52363c
Brian <briand.nester@gmail.com>
2021-03-05 13:54:06 -0500

    push

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const http = require('http');
+// const http = require('http');
 const app = express();
 
 const api = require('./server/routes/api')
@@ -17,10 +17,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/hackeramp/index.html'));
 });
 
-const port = process.env.PORT || '3000';
-app.set('port', port);
+// const port = process.env.PORT || '3000';
+// app.set('port', port);
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+// server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
