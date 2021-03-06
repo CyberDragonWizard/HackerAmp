57f72ab073027a38b2b5e378ef8ffa1eafb4bd9e
Brian <briand.nester@gmail.com>
2021-03-06 12:57:20 -0500

    push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
-// require("dotenv").config({ path: "./config.env"});
+require("dotenv").config({ path: "./config.env"});
 
 const api = require('./server/routes/api')
 
@@ -14,17 +14,17 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', api);
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '/dist/hackeramp')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/dist/hackeramp')));
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'dist', 'hackeramp', 'index.html'))
-//     })
-// } else {
-//     app.get('/posts', (req, res) => {
-//         res.send('API Running.')
-//     })
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist', 'hackeramp', 'index.html'))
+    })
+} else {
+    app.get('/api/posts', (req, res) => {
+        res.send('API Running.')
+    })
+}
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
