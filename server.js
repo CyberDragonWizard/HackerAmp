83b097ea09ba2a3458d83fc2f8c7c864088ea4a6
Brian <briand.nester@gmail.com>
2021-03-07 14:14:32 -0500

    Depolyment test

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.use('/api', api);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/dist/hackeramp')));
 
-    app.get('/api/posts', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'hackeramp', 'index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist/hackeramp/index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API Running')
     })
 }
 
